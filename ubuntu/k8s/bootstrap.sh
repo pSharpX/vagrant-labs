0e4fd15fb933e28c162dbf0d99fe77acda27c9a4
psharpx <crivera2093@gmail.com>
2021-09-05 12:32:18 -0500

    k8s cluster in ubuntu

--- ubuntu/k8s/bootstrap.sh
+++ ubuntu/k8s/bootstrap.sh
@@ -13,19 +13,16 @@ EOF
 
 install_tools() {
   apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release vim net-tools
+
+  # if we see this when execute kubeadm init, then install
+  # [preflight] WARNING: ebtables not found in system path
+  # apt-get install ebtables ethtool
+  # yum install ebtables ethtool
 }
 
 disable_swap() {
   swapoff -a
   sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-}
-
-activate_kubernetes_config() {
-cat <<EOF >>  /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
 }
 
 enable_docker_repo() {
@@ -40,11 +37,40 @@ install_docker() {
   apt-get remove docker docker-engine docker.io containerd runc
   apt-get install -y docker-ce docker-ce-cli containerd.io
 
+  # Create daemon json config file
+tee /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
   groupadd docker
   usermod -aG docker $USER
   usermod -aG docker vagrant
 
-  systemctl enable --now docker
+# Start and enable Services
+  systemctl daemon-reload
+  systemctl restart docker
+  systemctl enable docker
+  #systemctl enable --now docker
+}
+
+activate_kubernetes_config() {
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF >>  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+  sysctl --system
 }
 
 enable_kubernetes_repo() {
@@ -55,6 +81,7 @@ enable_kubernetes_repo() {
 
 install_kubernetes() {
   apt-get install -y kubelet kubeadm kubectl
+  # this option used to mark a package as held back, which will block the package from being installed, upgraded or removed.
   apt-mark hold kubelet kubeadm kubectl
 
   systemctl enable --now kubelet
@@ -74,7 +101,7 @@ lsb_release -ds
 
 # Update package list and upgrade all packages
 # sudo apt update && sudo apt upgrade -y
-apt update && apt upgrade -y
+# apt update && sudo apt upgrade -y
 
 # Preparing VM for provisioning a kubernetes cluster
 
@@ -85,14 +112,17 @@ set_hostnames
 disable_swap
 
 # 3.
-install_tools
+activate_kubernetes_config
 enable_docker_repo
-install_docker
+enable_kubernetes_repo
 
 # 4.
-activate_kubernetes_config
-enable_kubernetes_repo
-#install_kubernetes
+apt-get update && apt-get upgrade -y
+
+# 5.
+install_tools
+install_docker
+install_kubernetes
 
 # Tag the provision time:
 date > "$PROVISIONED_ON"
